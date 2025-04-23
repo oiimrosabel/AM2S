2b718bd2ce6333af58b7b9560ebd29bec5789dc5
Rosabel <imalonelynerd@gmail.com>
2025-04-23 02:06:39 +0200

    Iter 7.1 : added zip procedure

--- AM2S/procedure/ProcedureStore.py
+++ AM2S/procedure/ProcedureStore.py
@@ -1,7 +1,9 @@
 from AM2S.procedure.DefaultProcedure import DefaultProcedure
 from AM2S.procedure.DoomProcedure import DoomProcedure
+from AM2S.procedure.ZipProcedure import ZipProcedure
 
 strToProcedures = {
     "default": DefaultProcedure(),
+    "zip": ZipProcedure(),
     "doom": DoomProcedure()
 }
